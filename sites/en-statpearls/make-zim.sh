aa462821b6efe42ed90f938a0877923c7964db1f
Tim Moody <tim@timmoody.com>
2020-11-15 16:19:01 -0500

    script to create zim

---
+++ sites/en-statpearls/make-zim.sh
@@ -0,0 +1,8 @@
+# glyphicons are used by bootstrap
+# still missing /library/www/html/modules/en-statpearls/fonts/glyphicons-halflings-regular.woff2
+
+zimwriterfs --welcome=index.html --favicon=assets/stat-pearls-logo.png --language="eng" --title=StatPearls \
+ --description="StatPearls Article Library with more than 8000 articles on medical illnesses and conditions." \
+ --creator="StatPearls" --publisher="Internet in a Box" \
+ --tags="Medical Reference;_category:medical;_pictures:yes;_videos:yes;_details:yes;_ftindex:yes" \
+ /library/www/html/modules/en-statpearls /Files/Scraped/statpearls_en_all_maxi_2020-11.zim

0289bf274cae6229311fc17971c58a5f15c58fad
Tim Moody <tim@timmoody.com>
2020-11-23 08:28:50 -0500

    working

--- sites/gcfglobal/make-cat.py
+++ sites/gcfglobal/make-cat.py
@@ -5,10 +5,34 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import string
+import uuid
+from copy import deepcopy
 import iiab.adm_lib as adm
 
-alpha_pages = []
 gcf_catalog = {}
+item_base = {
+      "rating": "",
+      "age_range": "adult",
+      "zip_ftp_url": "",
+      "module_id": "xxx",
+      "is_hidden": "No",
+      "moddir": "en-xxx",
+      "category": "xxx",
+      "title": "",
+      "prereq_id": "",
+      "version": "1.0",
+      "ksize": "0",
+      "logo_url": "",
+      "type": "html",
+      "description": "",
+      "index_mod_sample_url": "",
+      "source_url": "",
+      "rsync_url": "",
+      "lang": "en",
+      "prereq_note": "",
+      "zip_http_url": "",
+      "file_count": "0"
+    }
 
 with open('download-all.html', 'r') as f:
     cat_html = f.read()
@@ -24,9 +48,29 @@ for topic in topics:
     print(category)
     items = topic.ul.find_all('li')
     for item in items:
-        name = item.a.text
+        title = item.a.text
+        print(title)
         zip_url = item.a['href']
-        gcf_catalog[name] = {'category': category, 'zip-url': zip_url}
+        moddir = zip_url.split('/')[-1]
+        moddir = moddir.split('.zip')[0]
+        if moddir[0:3] == 'tr_':
+            moddir = moddir[3:]
+        else:
+            moddir = 'en-' + moddir
+
+        item_info = {}
+        item_info = deepcopy(item_base)
+
+        item_info['module_id'] = str(uuid.uuid4())
+        item_info['moddir'] = moddir
+        item_info['category'] = category
+        item_info['title'] = title
+        item_info['category'] = category
+        item_info['lang'] = moddir[0:2] # may not work for Chinese
+        item_info['category'] = category
+        item_info['zip_http_url'] = zip_url
+
+        gcf_catalog[moddir] = item_info
 
 
 adm.write_json_file(gcf_catalog, 'gcf-catalog.json')

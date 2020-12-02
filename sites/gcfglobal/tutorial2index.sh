977cf889b3248632bc940b64825c4501507873b4
George Hunt <georgejhunt@gmail.com>
2020-12-02 16:36:49 +0000

    put this effort on ice. Kiwix wins

--- sites/gcfglobal/tutorial2index.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-for f in $(find .|grep tutorial.html); do
-  fnew=`echo $f | sed 's/_h.png/_half.png/'`
-  echo mv $fnew $(dirname $f)/index.html
-  mv $fnew $(dirname $f)/index.html
-done

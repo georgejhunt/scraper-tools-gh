760f2be0c569ab853a0ea15d1e3639d77c437d4c
George Hunt <georgejhunt@gmail.com>
2020-12-24 20:46:50 +0000

    once offline, lots of broken links

--- sites/gcfglobal/mk-gcf.sh
+++ sites/gcfglobal/mk-gcf.sh
@@ -2,7 +2,7 @@
 # The scraping of gcfglobal may be easy enough to do in bash
 
 WORKDIR=/library/working/rachel/gcf
-DISPDIR=/library/www/html/gjh/scraper-tools/sites/gcfglobal/test
+DISPDIR=/library/www/html/modules/en-GCF2020
 mkdir -p $WORKDIR/zip-files
 
 # Get the current download page which has the names o the zip files
@@ -34,7 +34,7 @@ for f in $(cat $WORKDIR/ziplist);do
    if [ ! -d $DISPDIR/en/$base ];then
       mkdir -p $DISPDIR/en/$base
       cd $DISPDIR/en/$base
-      echo $DISPDIR/en/$base
+      echo unzipping $basename into $DISPDIR/en/$base
       /usr/bin/unzip -q $WORKDIR/zip-files/$basename
    fi
 done
@@ -43,32 +43,16 @@ cd $curdir
 # I was never able to download scripts, style, images in the root
 # So I used a recursive wget -- mostly to get the lay of the land
 GCF_TREE=/library/www/html/modules/oneline
-mkdir -p GCF_TREE
-#wget -nHc -r -P $GCF_TREE/ -k -K --page-requisites https://edu.gcfglobal.org/
+mkdir -p $GCF_TREE
+if [ ! -f $GCF_TREE/index.html
+      wget -nHc -r -P $GCF_TREE/ -k -K --page-requisites https://edu.gcfglobal.org/
+fi
 /bin/cp -rpf $GCF_TREE/scripts $DISPDIR
 /bin/cp -rpf $GCF_TREE/styles $DISPDIR
 /bin/cp -rpf $GCF_TREE/images $DISPDIR
-mkdir -p $GCF_TREE/en/topics
+
+mkdir -p $DISPDIR/en/topics
 /bin/cp -rpf $GCF_TREE/en/topics/* $DISPDIR/en/topics/
-
-# copy in my own hand crafted landing page
-cd $DISPDIR
-/bin/cp -rpf ../homepage.index.html index.html
-
-function dummy(){
-   # get the topic directory from the live site which has all the menus
-   if [ ! -d "$DISPDIR/en/topics" ]; then
-      echo downloading topics
-      mkdir -p $DISPDIR/en/topics
-      wget -nH -r -P $DISPDIR/ -I en/topics,/en/subjects  https://edu.gcfglobal.org/
-   fi
-
-   # get the helper directories from the live site
-   if [ ! -d "$DISPDIR/scripts" ]; then
-      echo downloading style,images,scripts
-      #wget -nH -r -P $DISPDIR/ -I styles,images,scripts https://edu.gcfglobal.org/
-   fi
-}
 
 # If tutorial.html is changed to index.html, the menu system works offline
 cd $DISPDIR/en/
@@ -87,16 +71,22 @@ for f in $(find .|egrep /.+/index.html); do
    sed -i -e's|src"/scripts/|src=../../scripts/|' $f
    sed -i -e's|src="/images/|src="../../images/|' $f
 done
+
 cd $DISPDIR/en/topics
 for f in $(find .|grep index.html); do
    sed -i -e's|/en/|../|' $f
 done
 
-# update the main index.html to be relative lins
+# copy in my own hand crafted landing page -- extracted from header dropdown
+/bin/cp -rpf homepage.index.html $DISPDIR/index.html
+# We need a nice logo on our landing page
+/bin/cp -rpf gcfglobal-color.png $DISPDIR/images/gcfglobal-color.png
+
+# update the main index.html to be relative links
    sed -i -e's|href="/styles/|href="./styles/|' $DISPDIR/index.html
    sed -i -e's|src="/images/|src="./images/|' $DISPDIR/index.html
    sed -i -e's|src="/scripts/|src="./scripts/|' $DISPDIR/index.html
    sed -i -e's|href="/en/|href="./en/|' $DISPDIR/index.html
-
-
+   sed -i -e's|https://media.gcflearnfree.org/global/gcfglobal-color.png|./images/gcfglobal-color.png|' $DISPDIR/index.html
+   sed -i -e's|https://media.gcflearnfree.org/assets/logo/logo.png|./images/gcfglobal-color.png|' $DISPDIR/index.html
 
